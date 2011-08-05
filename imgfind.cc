51369a49ef9db13cf74e75f6aad9690568234947
Joseph Woodruff <jawoodru@cs.pdx.edu>
2011-08-05 04:42:16 -0700

    used autoproject to generate project skeleton

--- imgfind.cc
+++ imgfind.cc
@@ -1,27 +1,106 @@
-#include <stdio.h>
-
 /* 
-    imgfind - Searches for similar image files.
-    Copyright (C) 2011 Joseph Woodruff
+   imgfind - Searches for similar image files.
 
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
+   Copyright (C) 2011 Joseph Woodruff
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2, or (at your option)
+   any later version.
 
-    You should have received a copy of the GNU General Public License along
-    with this program; if not, write to the Free Software Foundation, Inc.,
-    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software Foundation,
+   Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.  
+
 */
 
-/* dummy file, replace later */
+#include <stdio.h>
+#include <sys/types.h>
+#include <getopt.h>
+#include "system.h"
 
-int main() {
-	printf("hello world\n");
-	return 0;
+#define EXIT_FAILURE 1
+
+extern "C" {
+  char *xstrdup (char *p);
+}
+
+static void usage (int status);
+
+/* The name the program was run with, stripped of any leading path. */
+char *program_name;
+
+/* Option flags and variables */
+
+
+static struct option const long_options[] =
+{
+  {"help", no_argument, 0, 'h'},
+  {"version", no_argument, 0, 'V'},
+  {NULL, 0, NULL, 0}
+};
+
+static int decode_switches (int argc, char **argv);
+
+int
+main (int argc, char **argv)
+{
+  int i;
+
+  program_name = argv[0];
+
+  i = decode_switches (argc, argv);
+
+  /* do the work */
+
+  exit (0);
+}
+
+/* Set all the option flags according to the switches specified.
+   Return the index of the first non-option argument.  */
+
+static int
+decode_switches (int argc, char **argv)
+{
+  int c;
+
+
+  while ((c = getopt_long (argc, argv, 
+			   "h"	/* help */
+			   "V",	/* version */
+			   long_options, (int *) 0)) != EOF)
+    {
+      switch (c)
+	{
+	case 'V':
+	  printf ("imgfind %s\n", VERSION);
+	  exit (0);
+
+	case 'h':
+	  usage (0);
+
+	default:
+	  usage (EXIT_FAILURE);
+	}
+    }
+
+  return optind;
+}
+
+
+static void
+usage (int status)
+{
+  printf (_("%s - Searches for similar image files.\n"), program_name);
+  printf (_("Usage: %s [OPTION]... [FILE]...\n"), program_name);
+  printf (_("Options:\n"
+"  -h, --help                 display this help and exit\n"
+"  -V, --version              output version information and exit\n"
+));
+  exit (status);
 }
