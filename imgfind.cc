a7c1f983f755fd641f09c1b99019a60a66cfc06e
Joseph Woodruff <jawoodru@cs.pdx.edu>
2011-08-05 08:17:34 -0700

    using autotools was a mistake

--- imgfind.cc
+++ imgfind.cc
@@ -21,86 +21,12 @@
 
 #include <stdio.h>
 #include <sys/types.h>
-#include <getopt.h>
-#include "system.h"
-
-#define EXIT_FAILURE 1
-
-extern "C" {
-  char *xstrdup (char *p);
-}
-
-static void usage (int status);
-
-/* The name the program was run with, stripped of any leading path. */
-char *program_name;
-
-/* Option flags and variables */
-
-
-static struct option const long_options[] =
-{
-  {"help", no_argument, 0, 'h'},
-  {"version", no_argument, 0, 'V'},
-  {NULL, 0, NULL, 0}
-};
-
-static int decode_switches (int argc, char **argv);
 
 int
 main (int argc, char **argv)
 {
-  int i;
-
-  program_name = argv[0];
-
-  i = decode_switches (argc, argv);
-
   /* do the work */
 
-  exit (0);
+  return 0;
 }
 
-/* Set all the option flags according to the switches specified.
-   Return the index of the first non-option argument.  */
-
-static int
-decode_switches (int argc, char **argv)
-{
-  int c;
-
-
-  while ((c = getopt_long (argc, argv, 
-			   "h"	/* help */
-			   "V",	/* version */
-			   long_options, (int *) 0)) != EOF)
-    {
-      switch (c)
-	{
-	case 'V':
-	  printf ("imgfind %s\n", VERSION);
-	  exit (0);
-
-	case 'h':
-	  usage (0);
-
-	default:
-	  usage (EXIT_FAILURE);
-	}
-    }
-
-  return optind;
-}
-
-
-static void
-usage (int status)
-{
-  printf (_("%s - Searches for similar image files.\n"), program_name);
-  printf (_("Usage: %s [OPTION]... [FILE]...\n"), program_name);
-  printf (_("Options:\n"
-"  -h, --help                 display this help and exit\n"
-"  -V, --version              output version information and exit\n"
-));
-  exit (status);
-}
