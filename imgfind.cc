63fbba8d9f244959212acbd40536994812b506aa
jawoodru@cs.pdx.edu <jawoodru@cs.pdx.edu>
2011-08-11 12:40:21 -0700

    updated header text to reflect license change

--- imgfind.cc
+++ imgfind.cc
@@ -5,7 +5,7 @@
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
-   the Free Software Foundation; either version 2, or (at your option)
+   the Free Software Foundation; either version 3, or (at your option)
    any later version.
 
    This program is distributed in the hope that it will be useful,
@@ -99,7 +99,7 @@ int main (int argc, char **argv)
     }
   
     // test criteria against hashlist; print path to matching files if found
-    matches = find_similar(search_criteria, dplist, 26);
+    matches = find_similar(search_criteria, dplist, 20);
     
     if (matches.empty()) {
         //no matches
